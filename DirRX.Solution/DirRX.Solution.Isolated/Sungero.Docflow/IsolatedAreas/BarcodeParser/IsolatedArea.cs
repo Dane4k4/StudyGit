f3dffc4d4847a9be9cab561b305b7ca6d5b0f586
d.sinev <d.sinev@centrvd.ru>
2025-11-07 09:49:14 +0300

    Обучение

---
+++ DirRX.Solution/DirRX.Solution.Isolated/Sungero.Docflow/IsolatedAreas/BarcodeParser/IsolatedArea.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Sungero.Core;
+using DirRX.Solution.Module.Docflow.Structures.Module;
+
+namespace DirRX.Solution.Module.Docflow.Isolated.BarcodeParser
+{
+
+}
---
+++ DirRX.Solution/DirRX.Solution.Isolated/Sungero.Docflow/IsolatedAreas/DocumentBodyComparer/IsolatedFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Sungero.Core;
+using DirRX.Solution.Module.Docflow.Structures.Module;
+
+namespace DirRX.Solution.Module.Docflow.Isolated.DocumentBodyComparer
+{
+  public partial class IsolatedFunctions
+  {
+
+  }
+}
